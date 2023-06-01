a917500944a925b1975b5407ec5cecd2c94853e9
nnewbury092423 <nnewbury9469@gmail.com>
2023-05-31 20:05:36 -0700

    final

--- estimate_gtr_pair.py
+++ estimate_gtr_pair.py
@@ -19,15 +19,23 @@ def gtr_params_pair(r, s, d):
     gtr_probs = dict() # keys: {'A', 'C', 'G', 'T'}   values: the corresponding GTR stationary probabilities
     gtr_rates = dict() # keys: {'AC', 'AG', 'AT', 'CG', 'CT', 'GT'}   values: the corresponding GTR transition rates
     # TODO Your code here
+
+
+    # Ordered keys for indexing purposes
     Orderedratekeys = ['AC', 'AG', 'AT', 'CG', 'CT', 'GT']
-    # Find R matrix
-    # Also count pis
+    
+    
+    
+    # Find R matrix and count stationary probabilities
     P = np.zeros((4,4))
     pis = np.zeros(4)
     for i in range(len(s)):
+
+        # fill in diagonal
         if(r[i] == s[i]):
             P[PROB_KEYS.index(r[i])][PROB_KEYS.index(r[i])] += 2
             pis[PROB_KEYS.index(r[i])] += 2
+        # fill in off diagonal
         else:
             try:
                 ind = Orderedratekeys.index(r[i] + s[i])
@@ -69,18 +77,16 @@ def gtr_params_pair(r, s, d):
                 P[3][2] +=1
                 pis[2] += 1
                 pis[3] += 1
+    # normalize P matrix and pis
     row_sum = P.sum(axis=1)
     P = P/row_sum[:,np.newaxis]
-    R = sp.linalg.logm(P)/d + np.exp(-15)
     pis = pis/pis.sum()
-    # Find rate changes and normalize
 
+
+
+    # Fill out dictionary
     # keys: {'A', 'C', 'G', 'T'} 
     #RATE_KEYS = ['CT', 'AT', 'GT', 'AC', 'CG', 'AG']
-
-
-
-
     gtr_rates['CT'] = R[3][1]/pis[1]
     gtr_rates['AT'] = R[3][0]/pis[0]
     gtr_rates['GT'] = R[3][2]/pis[2]
@@ -88,6 +94,8 @@ def gtr_params_pair(r, s, d):
     gtr_rates['CG'] = R[2][1]/pis[1]
     gtr_rates['AG'] = R[2][0]/pis[0]
     norm = gtr_rates['AG']
+
+    # normalize rates
     for key, value in gtr_rates.items():
         gtr_rates[key] = value/norm
 
@@ -96,10 +104,8 @@ def gtr_params_pair(r, s, d):
     for i in range(4):
         gtr_probs[PROB_KEYS[i]] = pis[i]
 
-
-    #import pdb; pdb.set_trace()
-
     return gtr_probs,gtr_rates
+
 
 def read_FASTA(filename):
     '''
